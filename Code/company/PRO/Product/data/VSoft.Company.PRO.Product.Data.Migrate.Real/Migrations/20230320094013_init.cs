37ffb0e28429d771a0df2758dcc664fc2626d127
lamit2708 <30747356+lamit2708@users.noreply.github.com>
2023-03-20 17:19:38 +0700

    add migration for keword of Product Table

---
+++ Code/company/PRO/Product/data/VSoft.Company.PRO.Product.Data.Migrate.Real/Migrations/20230320094013_init.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VSoft.Company.PRO.Product.Data.Migrate.Real.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Comment this code, run update-database to update this migration has done; then uncomment this code;
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<double>(type: "double", nullable: false),
+                    Quatity = table.Column<int>(type: "int(11)", nullable: false),
+                    Category = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true, defaultValueSql: "'NULL'"),
+                    Description = table.Column<string>(type: "varchar(512)", maxLength: 512, nullable: true, defaultValueSql: "'NULL'")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.Id);
+                })
+                .Annotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Code/company/PRO/Product/data/VSoft.Company.PRO.Product.Data.Migrate.Real/Migrations/20230320095001_addKeywordProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VSoft.Company.PRO.Product.Data.Migrate.Real.Migrations
+{
+    /// <inheritdoc />
+    public partial class addKeywordProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Keyword",
+                table: "Product",
+                type: "varchar(512)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Keyword",
+                table: "Product");
+        }
+    }
+}
